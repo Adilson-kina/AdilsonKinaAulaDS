d97173d8663bb3a4585879181c305203d7beabac
Adilson-kina <134547014+Adilson-kina@users.noreply.github.com>
2024-04-04 15:24:25 -0300

    Ex 3 While

---
+++ Projects/ParImpar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ParImpar{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int[] impar = new int[5];
+    for (int i = 0; i < 5; i++) {
+      int[i] = input.nextInt();
+    }
+    for (i = 0; i < 5, i++){
+      System.out.printf("%d ", int[i])
+    
+    }
+  }
+}
