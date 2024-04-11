f2faecd1cadabc4aa3c70304e3f34f6ebcad9725
Adilson-kina <134547014+Adilson-kina@users.noreply.github.com>
2024-04-11 14:32:18 -0300

    Tabuada for

---
+++ Projects/Tabuada.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ *
+ * Autor: RA: 1124736840 Nome: Adilson
+ * Data: 11/04/2024
+ *
+ **/
+
+public class Tabuada{
+  public static void main(String[] args){
+    System.out.print("Digite o numero para saber a tabuada:");
+    Scanner input = new Scanner(System.in);
+    int num = input.nextInt();
+    System.out.print("Digite até onde a tabuada deve ir:");
+    int ate_quando = input.nextInt();
+    for (int i = 1; i <= ate_quando; i++) {
+      System.out.printf("%d x %d = %d \n", num, i, i * num);
+    }
+    input.close();
+  }
+}
