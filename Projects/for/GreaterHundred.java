2aff827c9440cd335d19f15238302bfdbeb61c5a
Adilson-kina <adilsonkinakina@gmail.com>
2024-04-17 21:02:36 -0300

    ex 9

---
+++ Projects/for/GreaterHundred.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class GreaterHundred{
+  public static void main(String[] args){
+    int index = 100 + 1, plus_hundred = index + 10;
+    while (index < plus_hundred){
+      System.out.println(index);
+      index++;
+    }
+  }
+}
