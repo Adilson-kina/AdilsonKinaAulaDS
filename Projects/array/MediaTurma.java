d7caf5d9fea9ff3843362d8679db0c6bc3e00618
Adilson-kina <adilsonkinakina@gmail.com>
2024-04-25 20:35:54 -0300

    array

--- Projects/array/MediaTurma.java
+++ Projects/array/MediaTurma.java
@@ -4,19 +4,27 @@ class MediaTurma{
   public static void main(String[] argv){
     Scanner input = new Scanner(System.in);
     float[][] notas = new float[5][3];
+    float media_turma = 0;
+    int maior_que_media = 0;
     for (int i = 0; i < 5; i++) {
       for (int i2 = 0; i2 < 2; i2++) {
         System.out.printf("Digite a nota %d do aluno %d:", i2+1, i+1);
         notas[i][i2] = input.nextFloat();
-        System.out.printf("i2:%d, i:%d\n", i2, i);
       }
     }
     for (int i3 = 0; i3 < 5; i3++) {
-      notas[i3][2] = (notas[i3][0] + notas[i3][1]) / 2;
+      notas[i3][2] = (float) (notas[i3][0] + notas[i3][1]) / 2;
     }
     for (int i4 = 0; i4 < 5; i4++) {
-      System.out.printf("Media aluno %d: %f\n", i4, notas[i4][2]);
+      media_turma += notas[i4][2];
     }
+    media_turma /= 5;
+    for (int i5 = 0; i5 < 5; i5++) {
+      if(notas[i5][2] > media_turma){
+        maior_que_media++;
+      }
+    }
+    System.out.printf("A nota media da turma é de:%.1f \ne o numero de alunos com nota maior que a media é de:%d\n", media_turma, maior_que_media);
     input.close();
   }
 }
