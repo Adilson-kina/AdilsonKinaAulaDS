e225b8df1c91ea96ad6bf97a2e152d17df3003e9
Adilson-kina <134547014+Adilson-kina@users.noreply.github.com>
2024-04-25 22:01:31 -0300

    array

--- Projects/array/MediaTurma.java
+++ Projects/array/MediaTurma.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 class MediaTurma{
   public static void main(String[] argv){
     Scanner input = new Scanner(System.in);
-    float[][] notas = new float[5][3];
+    float[][] notas = new float[20][3];
     float media_turma = 0;
     int maior_que_media = 0;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 20; i++) {
       for (int i2 = 0; i2 < 2; i2++) {
         System.out.printf("Digite a nota %d do aluno %d:", i2+1, i+1);
         notas[i][i2] = input.nextFloat();
       }
     }
-    for (int i3 = 0; i3 < 5; i3++) {
+    for (int i3 = 0; i3 < 20; i3++) {
       notas[i3][2] = (float) (notas[i3][0] + notas[i3][1]) / 2;
     }
-    for (int i4 = 0; i4 < 5; i4++) {
+    for (int i4 = 0; i4 < 20; i4++) {
       media_turma += notas[i4][2];
     }
-    media_turma /= 5;
-    for (int i5 = 0; i5 < 5; i5++) {
+    media_turma /= 20;
+    for (int i5 = 0; i5 < 20; i5++) {
       if(notas[i5][2] > media_turma){
         maior_que_media++;
       }
